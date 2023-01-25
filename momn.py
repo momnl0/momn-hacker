883a412158f6c67aecead00b4327b4f79eccbc6b
momnl0 <123549325+momnl0@users.noreply.github.com>
2023-01-25 14:00:36 +0300

    Update momn.py

--- momn.py
+++ momn.py
@@ -1,8 +1,9 @@
-import os 
+import os
 os.system("clear") 
 os.system("apt update & apt upgrade") 
 os.system("pkg update")
 os.system("pkg install git") 
 os.system("git clone https://github.com/momnl0/momn-hacker")
 os.system("pkg install python & pkg install python2 & pkg install python3") 
-os.system("") 
+os.system("clear") 
+os.system("python") 
